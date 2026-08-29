732849aae8a20a4b1aa72c195acec1a1705b7a9c
rganel <rotemganel@gmail.com>
2026-08-28 22:11:38 -0400

    Basis for multiple maps

--- Assets/Scripts/ParallaxEnvironment.cs
+++ Assets/Scripts/ParallaxEnvironment.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +13,22 @@ public class ParallaxEnvironment : MonoBehaviour
         public Image FirstImage;
         public Image SecondImage;
         public float RelativeScrollSpeed;
+        public Material[] LevelMaterials;
+
+        public bool SetLevel(int level)
+        {
+            Debug.Assert(level >= 0);
+            Debug.Assert(LevelMaterials.Length > level);
+
+            FirstImage.material = LevelMaterials[level];
+
+            if (SecondImage != null)
+            {
+                SecondImage.material = LevelMaterials[level];
+            }
+
+            return true;
+        }
     }
 
     private void Awake()
@@ -19,13 +36,39 @@ public class ParallaxEnvironment : MonoBehaviour
         Debug.Assert(Layers != null);
     }
 
+    [ContextMenu("SetLevel1")]
+    public void SetLevel1()
+    {
+        SetLevel(1);
+    }
+    
+    [ContextMenu("SetLevel2")]
+    public void SetLevel2()
+    {
+        SetLevel(2);
+    }
+
+    public void SetLevel(int level)
+    {
+        Layers.ToList().ForEach(layer => layer.SetLevel(level));
+    }
+
     public void ApplyMovement(float baseMovement)
     {
         foreach (Layer layer in Layers)
         {
+            if (layer.RelativeScrollSpeed == 0)
+            {
+                continue;
+            }
+            
             Vector3 translation = Vector3.right * (baseMovement * layer.RelativeScrollSpeed);
             TranslateAndWrap(layer.FirstImage, translation);
-            TranslateAndWrap(layer.SecondImage, translation);
+
+            if (layer.SecondImage != null)
+            {
+                TranslateAndWrap(layer.SecondImage, translation);
+            }
         }
     }
 
